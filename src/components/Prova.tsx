56138aed490aaf0d27f1c76d4754bdf48a3437f5
Lorenzo <lforesta@pccube.com>
2024-12-11 16:31:43 +0100

    cambiato export

--- src/components/Prova.tsx
+++ src/components/Prova.tsx
@@ -5,7 +5,8 @@ export interface IButtonProps {};
 const Prova: React.FC<IButtonProps> = () => {
     return (
         <button>bottone</button>
-    )
-}
+    );
+};
 
-export default Prova;
+export { Prova };
+
